9f991f32bbbe3f1b4d515203066693b7a8878fcb
mikaelengver <mikael.engver@gmail.com>
2014-11-26 01:41:15 +0100

    Tutorial 6 - not complete yet

---
+++ filelistmodule.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+module.exports = function(path, extension, next) {
+	return { 
+		readDirectoryAsync : function() {
+			fs.readdir(path, function callBack(err, list) {
+				if (err) return next(err);
+				var fileList = "";
+				for (var i = 0; i < list.length; i++) {
+					if (endsWith(list[i], '.' + extension)) {
+						fileList += list[i] + '\r\n';
+					}
+				};
+				next(err, fileList);
+			});
+		}
+	}
+}
---
+++ program6.js
@@ -0,0 +1,21 @@
+var fileListModule = require('./filelistmodule');
+
+if (process.argv.length < 3) {
+	console.log('Supply path.');
+	return;
+}
+
+if (process.argv.length < 4) {
+	console.log('Supply filtering.');
+	return;
+}
+
+var path = process.argv[2];
+var extension = process.argv[3];
+var callback = function (err, fileListData) {
+	if (err) return err;
+	console.log(fileListData);
+}
+
+var myFileListModule = fileListModule(path, extension, callback);
+myFileListModule.readDirectoryAsync();
